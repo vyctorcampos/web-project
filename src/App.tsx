12ff440d85a9ecbcfd5b15d1223762f73212e7d5
vyctorcampos <vyctorcampos@hotmail.com>
2023-11-08 10:09:46 -0300

    config project and hello world

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react';
+
+export function App() {
+
+  return (
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">olá mundo</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">desenvolvendo header ihhhuuuu !!!</span>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+            </Button>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
